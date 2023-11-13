32d8d0113b76829e253f0fcd6a829e2c4ee07766
riddhi <riddhivekariya3@gmail.com>
2023-11-13 12:41:29 +0100

    Image Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import Image from "next/image";
 import Header from "./components/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <main className="flex items-center my-10">
+        <Image
+          className="ml-10"
+          src="/w&s_image.jpg"
+          alt="w&s_image"
+          width={800}
+          height={800}
+        />
+      </main>
     </>
   );
 }
